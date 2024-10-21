5706f19668d2652dfca6f3dda20444ad04aa67d8
bokhuuu <zurabokhua@gmail.com>
2024-10-21 14:18:45 +0400

    Add pop method to LinkedList with tests for edge cases: empty list, single node, and multiple nodes

--- data-structures/linked-lists.js
+++ data-structures/linked-lists.js
@@ -25,7 +25,37 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  pop() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    let prev = this.head;
+
+    while (temp.next) {
+      prev = temp;
+      temp = temp.next;
+    }
+
+    this.tail = prev;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(1);
 myLinkedList.push(2);
+
+// Tests for pop method
+console.log(myLinkedList.pop()); // Should log the node with value 2
+console.log(myLinkedList); // Should show that myLinkedList now has 1 node
+
+console.log(myLinkedList.pop()); // Should log the node with value 1
+console.log(myLinkedList); // Should show that myLinkedList is now empty
+
+console.log(myLinkedList.pop()); // Should log undefined (list is now empty)
