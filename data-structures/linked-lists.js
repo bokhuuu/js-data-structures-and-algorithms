fa8c16de7a9af2a1439451b383b23d007a95ec43
bokhuuu <zurabokhua@gmail.com>
2024-10-22 12:32:05 +0400

    Add shift method to LinkedList

--- data-structures/linked-lists.js
+++ data-structures/linked-lists.js
@@ -59,8 +59,22 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  shift() {
+    if (!this.head) return undefined;
+    let temp = this.head;
+    this.head = this.head.next;
+    temp.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return temp;
+  }
 }
 
 let myLinkedList = new LinkedList(1);
 myLinkedList.push(2);
 myLinkedList.unshift(0);
+myLinkedList.shift();
+myLinkedList;
