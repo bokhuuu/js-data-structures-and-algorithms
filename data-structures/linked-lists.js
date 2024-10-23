d8c3ade7d21ab1fcc8f4f1a36dca9094313b25c0
bokhuuu <zurabokhua@gmail.com>
2024-10-23 11:58:50 +0400

    Add insert(index, value) method to LinkedList for inserting at specific index

--- data-structures/linked-lists.js
+++ data-structures/linked-lists.js
@@ -90,6 +90,18 @@ class LinkedList {
       return false;
     }
   }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return this.push(value);
+    if (index === 0) return this.unshift(value);
+    let newNode = new Node(value);
+    let temp = this.get(index - 1);
+    newNode.next = temp.next;
+    temp.next = newNode;
+    this.length++;
+    return true;
+  }
 }
 
 let myLinkedList = new LinkedList(1);
@@ -100,4 +112,7 @@ myLinkedList;
 myLinkedList.get(1);
 myLinkedList.push(4);
 myLinkedList.set(2, 3);
+myLinkedList;
+myLinkedList;
+myLinkedList.insert(2, 4);
 myLinkedList;
